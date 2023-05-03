bb9932d3a60ef87f73f4f5555704ba23446c94e7
Zoe Meeker <zsm190001@utdallas.edu>
2023-05-03 00:54:51 -0500

    WIP Pixel Bars Designs & HP/MP Bar Scripts
    Started designing HUD layout - found all the assets/made sprites for the pixel version of the HUD - made HP bar fully functional - began working on scripts for the HP & MP bars

---
+++ Assets/Scripts/EnergyBar.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class EnergyBar : MonoBehaviour
+{
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
+
+
+    public void SetMaxEnergy(int energy)
+    {
+        slider.maxValue = energy;
+        slider.value = energy;
+        fill.color = gradient.Evaluate(1f);
+    }
+    public void SetEnergy(int energy)
+    {
+        slider.value = energy;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
+    }
+}
